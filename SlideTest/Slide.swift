7a66ddce85240a931963a7677665083d6a774190
Kabir <Kabir@focus4people.com>
2019-06-26 14:36:26 +0100

    First commit

---
+++ SlideTest/Slide.swift
@@ -0,0 +1,21 @@
+//
+//  Slide.swift
+//  SlideTest
+//
+//  Created by Focus 4 People on 25/06/2019.
+//  Copyright © 2019 Focus 4 People. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+  
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var labelTitle: UITextView!
+    
+    @IBOutlet weak var labelDesc: UITextView!
+    
+    
+}
--- SlideTest/ViewController.swift
+++ SlideTest/ViewController.swift
@@ -8,13 +8,184 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!{
+        didSet{
+            scrollView.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
+    
+    
+    var slides:[Slide] = [];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        slides = createSlides()
+        setupSlideScrollView(slides: slides)
+        
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        view.bringSubviewToFront(pageControl)
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    func createSlides() -> [Slide] {
+        
+        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide1.imageView.image = UIImage(named: "One")
+        slide1.labelTitle.text = "You should only compete with one person: yourself"
+        slide1.labelDesc.text = "Life is a competition, but it’s not a race against anyone else. Rather, the real journey is only against yourself and unrealized potential. This is what Jay Z means when he raps, “I look in the mirror, my only opponent.”"
+        
+        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide2.imageView.image = UIImage(named: "Two")
+        slide2.labelTitle.text = "Discover ideas about Life Is Short"
+        slide2.labelDesc.text = "U must learn to appreciate and respect everyone. Everyone has a right to be happy."
+        
+        let slide3:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide3.imageView.image = UIImage(named: "Three")
+        slide3.labelTitle.text = "You're All I Need to Get By"
+        slide3.labelDesc.text = "You're all that I need, I'll be there for you If you keep it real with me, I'll keep it real witchu Loving your whole schemes, it be in there boo On top of that you got the good Power-you"
+        
+        let slide4:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide4.imageView.image = UIImage(named: "Four")
+        slide4.labelTitle.text = "The Time Will Never Be ‘Just Right’"
+        slide4.labelDesc.text = "“Do not wait; the time will never be ‘just right.’ Start where you stand, and work with whatever tools you may have at your command, and better tools will be found as you go along.” — George Herbert"
+        
+        
+        let slide5:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide5.imageView.image = UIImage(named: "Five")
+        slide5.labelTitle.text = "Don’t stick with what’s pretty – strive for experimentation"
+        slide5.labelDesc.text = "t’s important to start any artistic project with a clear mind and without expectations. If you try to visualise and build your project in your mind in advance, there is a great chance of disappointment"
+        
+        let slide6:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide6.imageView.image = UIImage(named: "Six")
+        slide6.labelTitle.text = "Don’t stick with what’s pretty – strive for experimentation"
+        slide6.labelDesc.text = "t’s important to start any artistic project with a clear mind and without expectations. If you try to visualise and build your project in your mind in advance, there is a great chance of disappointment"
+        
+        let slide7:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide7.imageView.image = UIImage(named: "Seven")
+        slide7.labelTitle.text = "Don’t stick with what’s pretty – strive for experimentation"
+        slide7.labelDesc.text = "t’s important to start any artistic project with a clear mind and without expectations. If you try to visualise and build your project in your mind in advance, there is a great chance of disappointment"
+        
+        let slide8:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide8.imageView.image = UIImage(named: "Eight")
+        slide8.labelTitle.text = "Don’t stick with what’s pretty – strive for experimentation"
+        slide8.labelDesc.text = "t’s important to start any artistic project with a clear mind and without expectations. If you try to visualise and build your project in your mind in advance, there is a great chance of disappointment"
+        
+        return [slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8]
+    }
+    
+    
+    func setupSlideScrollView(slides : [Slide]) {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        scrollView.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.addSubview(slides[i])
+        }
+    }
+    
+    
+    /*
+     * default function called when view is scolled. In order to enable callback
+     * when scrollview is scrolled, the below code needs to be called:
+     * slideScrollView.delegate = self or
+     */
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+        
+        let maximumHorizontalOffset: CGFloat = scrollView.contentSize.width - scrollView.frame.width
+        let currentHorizontalOffset: CGFloat = scrollView.contentOffset.x
+        
+        // vertical
+        let maximumVerticalOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
+        let currentVerticalOffset: CGFloat = scrollView.contentOffset.y
+        
+        let percentageHorizontalOffset: CGFloat = currentHorizontalOffset / maximumHorizontalOffset
+        let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
+        
+        
+        /*
+         * below code changes the background color of view on paging the scrollview
+         */
+        //        self.scrollView(scrollView, didScrollToPercentageOffset: percentageHorizontalOffset)
+        
+        
+        /*
+         * below code scales the imageview on paging the scrollview
+         */
+        let percentOffset: CGPoint = CGPoint(x: percentageHorizontalOffset, y: percentageVerticalOffset)
+        
+        if(percentOffset.x > 0 && percentOffset.x <= 0.25) {
+            
+            slides[0].imageView.transform = CGAffineTransform(scaleX: (0.25-percentOffset.x)/0.25, y: (0.25-percentOffset.x)/0.25)
+            slides[1].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.25, y: percentOffset.x/0.25)
+            
+        } else if(percentOffset.x > 0.25 && percentOffset.x <= 0.50) {
+            slides[1].imageView.transform = CGAffineTransform(scaleX: (0.50-percentOffset.x)/0.25, y: (0.50-percentOffset.x)/0.25)
+            slides[2].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.50, y: percentOffset.x/0.50)
+            
+        } else if(percentOffset.x > 0.50 && percentOffset.x <= 0.75) {
+            slides[2].imageView.transform = CGAffineTransform(scaleX: (0.75-percentOffset.x)/0.25, y: (0.75-percentOffset.x)/0.25)
+            slides[3].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.75, y: percentOffset.x/0.75)
+            
+        } else if(percentOffset.x > 0.75 && percentOffset.x <= 1) {
+            slides[3].imageView.transform = CGAffineTransform(scaleX: (1-percentOffset.x)/0.25, y: (1-percentOffset.x)/0.25)
+            slides[4].imageView.transform = CGAffineTransform(scaleX: percentOffset.x, y: percentOffset.x)
+        }
+    }
+    
+    
+    
+    
+    func scrollView(_ scrollView: UIScrollView, didScrollToPercentageOffset percentageHorizontalOffset: CGFloat) {
+        if(pageControl.currentPage == 0) {
+            //Change background color to toRed: 103/255, fromGreen: 58/255, fromBlue: 183/255, fromAlpha: 1
+            //Change pageControl selected color to toRed: 103/255, toGreen: 58/255, toBlue: 183/255, fromAlpha: 0.2
+            //Change pageControl unselected color to toRed: 255/255, toGreen: 255/255, toBlue: 255/255, fromAlpha: 1
+            
+            let pageUnselectedColor: UIColor = fade(fromRed: 255/255, fromGreen: 255/255, fromBlue: 255/255, fromAlpha: 1, toRed: 103/255, toGreen: 58/255, toBlue: 183/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
+            pageControl.pageIndicatorTintColor = pageUnselectedColor
+            
+            
+            let bgColor: UIColor = fade(fromRed: 103/255, fromGreen: 58/255, fromBlue: 183/255, fromAlpha: 1, toRed: 255/255, toGreen: 255/255, toBlue: 255/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
+            slides[pageControl.currentPage].backgroundColor = bgColor
+            
+            let pageSelectedColor: UIColor = fade(fromRed: 81/255, fromGreen: 36/255, fromBlue: 152/255, fromAlpha: 1, toRed: 103/255, toGreen: 58/255, toBlue: 183/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
+            pageControl.currentPageIndicatorTintColor = pageSelectedColor
+        }
+    }
+    
+    
+    func fade(fromRed: CGFloat,
+              fromGreen: CGFloat,
+              fromBlue: CGFloat,
+              fromAlpha: CGFloat,
+              toRed: CGFloat,
+              toGreen: CGFloat,
+              toBlue: CGFloat,
+              toAlpha: CGFloat,
+              withPercentage percentage: CGFloat) -> UIColor {
+        
+        let red: CGFloat = (toRed - fromRed) * percentage + fromRed
+        let green: CGFloat = (toGreen - fromGreen) * percentage + fromGreen
+        let blue: CGFloat = (toBlue - fromBlue) * percentage + fromBlue
+        let alpha: CGFloat = (toAlpha - fromAlpha) * percentage + fromAlpha
+        
+        // return the fade colour
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
 }
 
